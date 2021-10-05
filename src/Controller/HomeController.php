ced5345ac879a02afba63351c884d28b6423f16a
matthieu Dupont <ma77hieu@github.com>
2021-10-05 13:28:33 +0200

    DB creation, property entity and repository created, display of property on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    public function __construct($twig)
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct($twig,PropertyRepository $repository)
     {
-        
+        $this->repository=$repository;
     }
     public function index(): Response
     {
+        
+        $properties=$this->repository->findLatest();
+        dump($properties);
         return $this->render('pages/home.html.twig',[
-            'current_menu'=>"home"
+            'current_menu'=>"home",
+            'properties'=>$properties
         ]);
     }
     
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ListProperties extends AbstractController
 {
-    public function __construct($twig)
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct($twig,PropertyRepository $repository,ManagerRegistry $em)
     {
-        
+        $this->repository=$repository;
+        $this->em=$em->getManager();
     }
     public function index(): Response
     {
+        // $property= new Property();
+        // $property->setTitle('Premier bien')
+        //     ->setPrice(200000) 
+        //     ->setRooms(4)
+        //     ->setBedrooms(3)
+        //     ->setDescription('une descirption standard')
+        //     ->setSurface(60)
+        //     ->setFloor(4)
+        //     ->setHeat(1)
+        //     ->setCity('Montpellier')
+        //     ->setAddress('15 boulevard Gambetta')
+        //     ->setPostalCode('34000');
+        // $em=$this->getDoctrine()->getManager();
+        // $em->persist($property);
+        // $em->flush();
+        // $property=$this->repository->findAllVisible();
+        // $property[0]->setSold(true);
+        // dump($property);
+        // $this->em->flush();
         return $this->render('property/property.html.twig',[
             'current_menu'=>"properties"
         ]);
